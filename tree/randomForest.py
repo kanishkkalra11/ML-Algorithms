2679dbb616401492ffa71741f47d26321704ed27
Kanishk Kalra <kanishkkalra1999@gmail.com>
2020-07-30 08:10:38 +0530

    Update randomForest.py

--- tree/randomForest.py
+++ tree/randomForest.py
@@ -2,6 +2,7 @@ from .base import DecisionTree
 from sklearn import tree
 import pandas as pd
 import numpy as np
+from metrics import *
 
 class RandomForestClassifier():
     def __init__(self, n_estimators=100, criterion='gini', max_depth=None):
@@ -146,5 +147,41 @@ class RandomForestRegressor():
         final_output = pd.Series(final_output)
 
         return final_output
+
+np.random.seed(42)
+
+########### RandomForestClassifier ###################
+
+N = 30
+P = 5
+X = pd.DataFrame(np.random.randn(N, P))
+y = pd.Series(np.random.randint(P, size = N), dtype="category")
+
+for criteria in ['entropy', 'gini']:
+    Classifier_RF = RandomForestClassifier(10, criterion = criteria)
+    Classifier_RF.fit(X, y)
+    y_hat = Classifier_RF.predict(X)
+    Classifier_RF.plot()
+    print('Criteria :', criteria)
+    print('Accuracy: ', accuracy(y_hat, y))
+    for cls in y.unique():
+        print('Precision: ', precision(y_hat, y, cls))
+        print('Recall: ', recall(y_hat, y, cls))
+
+########### RandomForestRegressor ###################
+
+N = 30
+P = 5
+X = pd.DataFrame(np.random.randn(N, P))
+y = pd.Series(np.random.randn(N))
+
+criteria = "mse"
+Regressor_RF = RandomForestRegressor(10, criterion = criteria)
+Regressor_RF.fit(X, y)
+y_hat = Regressor_RF.predict(X)
+Regressor_RF.plot()
+print('Criteria :', criteria)
+print('RMSE: ', rmse(y_hat, y))
+print('MAE: ', mae(y_hat, y))
 
    
